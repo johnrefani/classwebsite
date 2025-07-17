782a18e6851f253a22d1339dd1971f8b2f73bf1e
John Cloyd Refani <jhnrefani@gmail.com>
2025-07-17 17:03:24 +0800

    Added Selection Page

---
+++ components/ui/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className="mt-6 w-[150px] py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+      PROCEED
+    </button>
+  )
+}
+
+export default Button
