35a33c7bd54c6d46fdbc803f982a5d3f3e2ab726
John Cloyd Refani <jhnrefani@gmail.com>
2025-07-25 15:55:58 +0800

    Added dropdown and dashboard

--- components/ui/StudentCard.tsx
+++ components/ui/StudentCard.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
 import Student from './Student';
+import Dropdown from './Dropdown';
 
 interface StudentClass{
   text: string,
 }
 const StudentCard = ({text}: StudentClass) => {
   return (
-    <div className='bg-white rounded-lg p-6 w-[500px] h-[600px]'>
-        <div className='w-full max-w-2xl'>
-            <h2 className='text-2xl font-semibold text-gray-950 mb-4'>{text}</h2>
+    <div className='bg-white rounded-lg p-6 w-[500px] h-[600px] border border-black'>
+        <div className='w-full max-w-2xl flex justify-between'>
+            <h2 className='text-2xl font-semibold text-gray-950 mb-4'>{text}</h2>           
+            <Dropdown/>
+
         </div>
         <div className='w-full border-black border py-3 mb-2 px-5 rounded-xl text-lg text-black outline-none flex items-center justify-start gap-1'>
             <IoSearch />
